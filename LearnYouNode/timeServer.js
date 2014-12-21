fc033304fe995c70c97d8aece627e813cb500ee7
Paul <paultomchik@gmail.com>
2014-12-21 18:29:32 -0500

    Completed Tim Server.

---
+++ LearnYouNode/timeServer.js
@@ -0,0 +1,22 @@
+(function() {
+  var portNum = process.argv[2];
+
+  function zeroPad(num) {
+    return ('0' + num).slice(-2);
+  }
+
+  function getTimeString () {
+    var date = new Date();
+
+    return date.getFullYear() + '-' +
+           zeroPad(1 + date.getMonth()) + '-' +
+           zeroPad(date.getDate()) + ' ' +
+           zeroPad(date.getHours()) + ':' +
+           zeroPad(date.getMinutes()) + '\n';
+  }
+
+  var server = require('net').createServer( function (socket) {
+    socket.end(getTimeString()); 
+  });
+  server.listen(portNum);
+})();
